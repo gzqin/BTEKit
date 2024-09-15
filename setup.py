e729e45c3f973c12fd37d9b50d1d929818d0609a
YiWei <94545667+wyafq@users.noreply.github.com>
2024-09-15 22:13:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ setup(
     name="BTEKit",
     version="0.1",
     packages=find_packages(),
-    include_package_data=True,  # 启用文件的自动包含
+    include_package_data=True,  
     package_data={
-        # 包含 'scripts' 目录下的所有 .sh 和其他文件
         'BTEKit': ['scripts/*.sh', 'scripts/*.py', 'scripts/*'],  
     },
     entry_points={
         "console_scripts": [
-            "btekit=BTEKit.main:main",  # BTEKit是命令名，BTEKit.main是Python模块，main是函数名
+            "btekit=BTEKit.main:main", 
         ],
     },
     install_requires=[
-        # 这里你可以列出任何必要的依赖项
     ],
     zip_safe=False,
     description="A toolkit for running scripts with easy selection interface",
